3aa536505d5c95d0236d12c9e395bfa574386cd0
Jinliang Zhu <zhujinlianghust@gmail.com>
2013-09-09 20:44:18 +0800

    Normalize something

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-Overview
-==============
+# Overview
+
 This script is for UCAS student to login the network in Chinese Academy
 of Sciences.
 
-Notice
-============
+# Notice
+
 * 若第一次使用，注意先在user_data中的username和password中修改自己的信息。
 * Python version: above 2.5 but lower than 3.
   If your Python version is above 3, it will report syntax error.
@@ -14,26 +14,48 @@ Notice
   display 10 seconds. So, maybe the next notification is delayed.
 
 
-How to use
-==============
+# How To Use
+## Synopsis
+
 Run it like:
-python cas_login.py option
+
+	python cas_login.py [option]
 
 You can only input one option.
 Options are as follows:
-0: 登出
-1: 连接城域
-2: 连接国内
-3: 连接国际
 
-For example: 
+	0: 登出
+	1: 连接城域
+	2: 连接国内
+	3: 连接国际
+
+## Example
 When you want to connect to international, input this:
-python cas_login.py 3
-When you want to logout, input this:
-python cas_login.py 0
 
-Update History
-============
+	python cas_login.py 3
+
+When you want to logout, input this:
+
+	python cas_login.py 0
+
+## Tips
+
+You can use this command to create a symbol link:
+
+	sudo ln -s (the absolute path of your cas_login dir)/cas_login.py /bin/cas_login
+
+Then, you can use `cas_login` as a Shell command to login UCAS net, like this:
+
+	cas_login 3
+
+
+# Update History
+
+Ver1.2, 9 Sep 2013
+	* Normalize all the unicode string.
+	* Normalize all the indentation.
+	* Update the README file.
+
 Ver1.1, 20 Jan 2013
 	* Add desktop notification when login or logout.
 
@@ -42,15 +64,15 @@ Ver1.0, 20 Dec 2012
 	* Three mode to login the net.
 
 
-Note
-=============
+# Note
+
 Since the author's ability is limited, the software may exist many bugs
 that I didn't have found and solve them. Please write them in a file, 
 and send to me if possible. I'll appreciate your work very much!
 
-Contact
-============
+# Contact
+
 If you want to make contact with me, mail me at this:
 zhujinlianghust#gmail.com
 
--Zhu Jinliang, Dec 2012
+-Jinliang Zhu, Dec 2012
--- cas_login.py
+++ cas_login.py
@@ -2,9 +2,8 @@
 # -*- coding:utf-8 -*-
 # Author: Zhu Jinliang
 # Email: zhujinlianghust#gmail.com
-"""
-This script is for login the network in Chinese Academy of Sciences.
-"""
+# Description: login the net in University of Chinese Academy of Sciences in command.
+
 # 若第一次使用，注意先在user_data中的username和password中修改自己的信息
 
 import sys
@@ -14,78 +13,81 @@ import urllib2
 import cookielib
 
 user_data = {
-			"username": "2012***", # 用你的用户名替换2012*** 
-			"password": "{TEXT}123456", # 用你的密码替换123456，注意保留{TEXT}
-			"drop": "3", 
-			"type": "1", 
-			"n": "100" 
-			}
+    'username': '2012***', # 用你的用户名替换2012*** 
+    'password': '{TEXT}123456', # 用你的密码替换123456，注意保留{TEXT}
+    'drop': '3', 
+    'type': '1', 
+    'n': '100' 
+}
 
 def notify(message):
-	image = "cas.png"
-	notify_command = "notify-send -i %s '%s'"% (image, message)
-	os.system(notify_command)
+    image = 'cas.png'
+    notify_command = 'notify-send -i %s '%s''% (image, message)
+    os.system(notify_command)
 
 
 def login():
-	"""
-	For user to login to web.
-	"""
-	host_url = "http://auth.ucas.ac.cn"
+    '''
+    For user to login to web.
+    '''
+    host_url = 'http://auth.ucas.ac.cn'
 
-	# 设置一个cookie处理器，负责从服务器下载cookie到本地，并且发送请求时带上本地cookie
-	cj = cookielib.LWPCookieJar()
-	cookie = urllib2.HTTPCookieProcessor(cj)
-	opener = urllib2.build_opener(cookie, urllib2.HTTPHandler)
-	urllib2.install_opener(opener)
-	# 打开一个登录主页面
-	h = opener.open(host_url)
-	return opener
+    cj = cookielib.LWPCookieJar()
+    cookie = urllib2.HTTPCookieProcessor(cj)
+    opener = urllib2.build_opener(cookie, urllib2.HTTPHandler)
+    urllib2.install_opener(opener)
+
+    h = opener.open(host_url)
+    return opener
 
 
 def use_net(opener, post_data):
-	"""
-	When user login the web, he can use net.
-	"""
-	login_url = "http://auth.ucas.ac.cn/cgi-bin/do_login"
-	headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:17.0) Gecko/20100101 Firefox/17.0', 
-			'Accept': 'pplication/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-			'Accept-Language': 'en-US,en;q=0.5',
-			'Accept-Encoding': 'gzip, deflate',
-			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-			'Referer': 'http://auth.ucas.ac.cn/'
-			 }
-	# 给post数据编码
-	post_data = urllib.urlencode(post_data)
-	# 发送POST请求
-	request = urllib2.Request(login_url, post_data, headers)
-	try:
-		response = opener.open(request)
-		msg =  user_data['username'] + " " +  "你已经成功登录"
-		print msg
-		notify(msg)
-	except urllib2.URLError:
-		msg =  user_data['username'] + " " + "登录失败"
-		print msg
-		notify(msg)
+    '''
+    When user login the web, he can use net.
+    '''
+    login_url = 'http://auth.ucas.ac.cn/cgi-bin/do_login'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:17.0) Gecko/20100101 Firefox/17.0', 
+        'Accept': 'pplication/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Referer': 'http://auth.ucas.ac.cn/'
+     }
+
+    post_data = urllib.urlencode(post_data)
+    request = urllib2.Request(login_url, post_data, headers)
+    try:
+        response = opener.open(request)
+        msg =  user_data['username'] + ' ' +  u'你已经成功登录'
+        print msg
+        notify(msg)
+    except urllib2.URLError:
+        msg =  user_data['username'] + ' ' + u'登录失败'
+        print msg
+        notify(msg)
 
 def logout(opener, user_data):
-	logout_url = 'http://auth.ucas.ac.cn/cgi-bin/do_logout'
-	opener.open(logout_url)
-	msg = user_data['username'] + " " + "你已经成功登出"
-	print msg
-	notify(msg)
+    logout_url = 'http://auth.ucas.ac.cn/cgi-bin/do_logout'
+    opener.open(logout_url)
+    msg = user_data['username'] + ' ' + u'你已经成功登出'
+    print msg
+    notify(msg)
 
 
-if __name__ == "__main__":
-	mode = {"0": "4",
-			"1": "1",
-			"2": "3",
-			"3": "0"}
-	display_mode = {"1": "城域",
-					"2": "国内",
-					"3": "国际"}	
-	help_info = """\
+if __name__ == '__main__':
+    mode = {
+        '0': '4',
+        '1': '1',
+        '2': '3',
+        '3': '0'
+    }
+    display_mode = {
+        '1': u'城域',
+        '2': u'国内',
+        '3': u'国际'
+    }  
+    help_info = '''\
 Usage: python cas_login.py [option]
 You can only input one option.
 Options are as follows:
@@ -99,24 +101,22 @@ When you want to connect to international, input this:
 python cas_login.py 3
 When you want to logout, input this:
 python cas_login.py 0
-"""
-	opener = login()
-	if (len(sys.argv) < 2) or (len(sys.argv) > 2):
-		print "Option Error. No action specified."
-		print help_info
-		sys.exit()
-	elif sys.argv[1] != "0": 
-	    if sys.argv[1] in mode:
-			option = sys.argv[1]
-			# Change connection mode
-			user_data["drop"] = mode[option]
-			msg = "连接%s..."% display_mode[option]
-			print msg
-			notify(msg)
-			use_net(opener, user_data)
-	else:
-		print "正在登出..."
-		notify("正在登出...")
-		logout(opener, user_data)
-
-
+'''
+    opener = login()
+    if (len(sys.argv) < 2) or (len(sys.argv) > 2):
+        print 'Option Error. No action specified.'
+        print help_info
+        sys.exit()
+    elif sys.argv[1] != '0': 
+        if sys.argv[1] in mode:
+            option = sys.argv[1]
+            # Change connection mode
+            user_data['drop'] = mode[option]
+            msg = u'连接%s...'% display_mode[option]
+            print msg
+            notify(msg)
+            use_net(opener, user_data)
+    else:
+        print u'正在登出...'
+        notify(u'正在登出...')
+        logout(opener, user_data)
